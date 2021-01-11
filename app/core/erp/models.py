832fa377f0544c9241e629e4c9646f24051a4c1f
Esau Gonzalez <gonesau20@gmail.com>
2021-01-11 16:57:45 -0600

    Creation of models

--- app/core/erp/models.py
+++ app/core/erp/models.py
@@ -1,23 +1,12 @@
 from django.db import models
 from datetime import datetime
-
-class Type(models.Model):
-    name = models.CharField(max_length=150, verbose_name='Nombre')
-
-    def __str__(self):
-        return self.name
-
-    class Meta:
-        verbose_name = 'Tipo'
-        verbose_name_plural = 'Tipos'
-        ordering = ['id']
-        db_table = 'tipo'
+from core.erp.choices import gender_choices
 
 class Category(models.Model):
-    name = models.CharField(max_length=150, verbose_name='Nombre')
+    name = models.CharField(max_length=150, verbose_name='Nombre', unique=True)
 
     def __str__(self):
-        return self.name
+        return 'Nombre: {}'.format(self.name)
 
     class Meta:
         verbose_name = 'Categoría'
@@ -25,29 +14,95 @@ class Category(models.Model):
         ordering = ['id']
         db_table = 'categoria'
 
-class Employee(models.Model):
-    #Relacionar Tablas
-    type = models.ForeignKey(Type, on_delete=models.CASCADE)
-    categ = models.ManyToManyField(Category)
+class Product(models.Model):
+    name = models.CharField(max_length=150, verbose_name='Nombre', unique=True)
+    cate = models.ForeignKey(Category, on_delete=models.CASCADE)
+    image = models.ImageField(upload_to='product/%Y/%m/%d', null=True, blank=True)
+    pvp = models.DecimalField(default=0.00, max_digits=9, decimal_places=2)
 
+    def __str__(self):
+        return self.name
+
+    class Metas:
+        verbose_name = 'Producto'
+        verbose_name_plural = 'Productos'
+        ordering = ['id']
+        db_table = 'producto'
+
+class Client(models.Model):
     names = models.CharField(max_length=150, verbose_name='Nombres')
-    last_name = models.CharField(max_length=150, verbose_name='Apellidos')
-    dui = models.CharField(max_length=10, verbose_name='DUI', unique=True)
-    date_joined = models.DateField(default=datetime.now, verbose_name='Fecha de Registro')
-    date_creation = models.DateTimeField(auto_now=True, verbose_name='Fecha de Creación')
-    date_update = models.DateTimeField(auto_now_add=True, verbose_name='Fecha de Actualización')
-    age = models.PositiveIntegerField(default=0, verbose_name='Edad')
-    salary = models.DecimalField(default=0.00, max_digits=9, decimal_places=2, verbose_name='Salario')
-    state = models.BooleanField(default=True, verbose_name='Estado')
-    gender = models.CharField(max_length=50, verbose_name='Sexo')
-    avatar = models.ImageField(upload_to='avatar/%Y/%m/%d', null=True, blank=True)
-    cvitae = models.FileField(upload_to='cvitae/%Y/%m/%d', null=True, blank=True)
+    last_names = models.CharField(max_length=150, verbose_name='Apellidos')
+    dui = models.CharField(max_length=10, unique=True, verbose_name='DUI')
+    nit = models.CharField(max_length=17, unique=True, verbose_name='NIT')
+    birtday = models.DateField(default=datetime.now, verbose_name='Fecha de Nacimiento')
+    address = models.CharField(max_length=150, null=True, blank=True, verbose_name='Dirección')
+    sexo = models.CharField(max_length=10, choices=gender_choices, default='male', verbose_name='Sexo')
 
     def __str__(self):
         return self.names
 
     class Meta:
-        verbose_name = 'Empleado'
-        verbose_name_plural = 'Empleados'
-        db_table = 'empleado'
+        verbose_name = 'Cliente'
+        verbose_name_plural = 'Clientes'
         ordering = ['id']
+        db_table = 'cliente'
+
+class Sale(models.Model):
+    cli = models.ForeignKey(Client, on_delete=models.CASCADE)
+    date_joined = models.DateField(default=datetime.now)
+    subtotal = models.DecimalField(default=0.00, max_digits=9, decimal_places=2)
+    iva = models.DecimalField(default=0.00, max_digits=9, decimal_places=2)
+    total = models.DecimalField(default=0.00, max_digits=9, decimal_places=2)
+
+    def __str__(self):
+        return self.cli.names
+
+    class Meta:
+        verbose_name = 'Venta'
+        verbose_name_plural = 'Ventas'
+        ordering = ['id']
+        db_table = 'venta'
+
+class DetSale(models.Model):
+    sale = models.ForeignKey(Sale, on_delete=models.CASCADE)
+    prod = models.ForeignKey(Product, on_delete=models.CASCADE)
+    price = models.DecimalField(default=0.00, max_digits=9, decimal_places=2)
+    cant = models.IntegerField(default=0)
+    subtotal = models.DecimalField(default=0.00, max_digits=9, decimal_places=2)
+
+    def __str__(self):
+        return self.prod.name
+
+    class Meta:
+        verbose_name = 'Detalle de Venta'
+        verbose_name_plural = 'Detalle de Ventas'
+        ordering = ['id']
+        db_table = 'detalle_venta'
+
+
+# class Employee(models.Model):
+#     #Relacionar Tablas
+#     type = models.ForeignKey(Type, on_delete=models.CASCADE)
+#     categ = models.ManyToManyField(Category)
+#
+#     names = models.CharField(max_length=150, verbose_name='Nombres')
+#     last_name = models.CharField(max_length=150, verbose_name='Apellidos')
+#     dui = models.CharField(max_length=10, verbose_name='DUI', unique=True)
+#     date_joined = models.DateField(default=datetime.now, verbose_name='Fecha de Registro')
+#     date_creation = models.DateTimeField(auto_now=True, verbose_name='Fecha de Creación')
+#     date_update = models.DateTimeField(auto_now_add=True, verbose_name='Fecha de Actualización')
+#     age = models.PositiveIntegerField(default=0, verbose_name='Edad')
+#     salary = models.DecimalField(default=0.00, max_digits=9, decimal_places=2, verbose_name='Salario')
+#     state = models.BooleanField(default=True, verbose_name='Estado')
+#     gender = models.CharField(max_length=50, verbose_name='Sexo')
+#     avatar = models.ImageField(upload_to='avatar/%Y/%m/%d', null=True, blank=True)
+#     cvitae = models.FileField(upload_to='cvitae/%Y/%m/%d', null=True, blank=True)
+#
+#     def __str__(self):
+#         return self.names
+#
+#     class Meta:
+#         verbose_name = 'Empleado'
+#         verbose_name_plural = 'Empleados'
+#         db_table = 'empleado'
+#         ordering = ['id']

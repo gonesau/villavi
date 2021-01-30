7cd4c6e30641126763d1e15aa27c3456bbda0496
Esau Gonzalez <gonesau20@gmail.com>
2021-01-29 23:38:47 -0600

    Ajax y POST

--- app/core/erp/views/category/views.py
+++ app/core/erp/views/category/views.py
@@ -1,6 +1,9 @@
+from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
 from django.views.generic import ListView
-
-from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from django.utils.decorators import method_decorator
+from django.shortcuts import render, redirect
 from core.erp.models import Category, Product
 
 
@@ -15,8 +18,18 @@ class CategoryListView(ListView):
     model = Category
     template_name = 'category/list.html'
 
-    def get_queryset(self):
-        return Category.objects.all()
+
+    @method_decorator(csrf_exempt)
+    def dispatch(self, request, *args, **kwargs):
+        return super().dispatch(request,*args, **kwargs)
+
+    def post(self, request, *args, **kwargs):
+        data = {}
+        try:
+            data = Category.objects.get(pk=request.POST['id']).toJSON()
+        except Exception as e:
+            data['error'] = str(e)
+        return JsonResponse(data)
 
     def get_context_data(self, *, object_list=None, **kwargs):
         context = super().get_context_data(**kwargs)

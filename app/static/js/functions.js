91feb37bbf735416a43a1386c9d4ce880ec05cd0
Esau Gonzalez <gonesau20@gmail.com>
2021-02-14 21:45:04 -0600

    Detalles guardado

--- app/static/js/functions.js
+++ app/static/js/functions.js
@@ -6,13 +6,58 @@ function message_error(obj) {
             html += '<li>' + key + ': ' + value + '</li>';
         });
         html += '</ul>';
-    }
-    else{
-        html = '<p>'+obj+'</p>';
+    } else {
+        html = '<p>' + obj + '</p>';
     }
     Swal.fire({
         title: 'Error!',
         html: html,
         icon: 'error'
     });
+}
+
+function submit_with_ajax(url, parameters, callback) {
+    $.confirm({
+        theme: 'material',
+        title: 'Confirmación',
+        icon: 'fa fa-info',
+        content: '¿Estás seguro de realizar la siguiente acción?',
+        columnClass: 'medium',
+        typeAnimated: true,
+        cancelButtonClass: 'btn-primary',
+        draggable: true,
+        dragWindowBorder: false,
+        buttons: {
+            info: {
+                text: "Si",
+                btnClass: 'btn-primary',
+                action: function () {
+                    $.ajax({
+                        url: url,
+                        type: 'POST',
+                        data: parameters,
+                        dataType: 'json'
+                    }).done(function (data) {
+                        console.log(data);
+                        if (!data.hasOwnProperty('error')) {
+                            callback();
+                            return false;
+                        }
+                        message_error(data.error);
+                    }).fail(function (jqXHR, textStatus, errorThrown) {
+                        alert(textStatus + ': ' + errorThrown);
+                    }).always(function (data) {
+
+                    });
+                }
+            },
+            danger: {
+                text: "No",
+                btnClass: 'btn-red',
+                action: function () {
+
+                }
+            },
+        }
+    })
 }
